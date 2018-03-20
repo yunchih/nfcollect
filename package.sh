e192f105d6d85814cb2a3b2abd8eae58ede858cf
Yunchih Chen <yunchih.cat@gmail.com>
2018-03-20 22:28:39 +0800

    Add package.sh and PKGBUILD for packaging

---
+++ package.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+PKGNAME=nfcollect
+PKGVER=0.1
+PKGREL=1
+PKGDIR=${PKGNAME}-${PKGVER}
+PKG="${PKGNAME}-${PKGVER}-${PKGREL}.tar.gz"
+
+echo "Packaging $PKG ..."
+echo
+
+mkdir -p "${PKGDIR}"
+cp -a bin lib include \
+      configure configure.ac build-aux \
+      Makefile.{in,am} \
+      service \
+      "${PKGDIR}"
+
+tar --exclude "*.swp" \
+    --exclude .deps \
+    --exclude .dirstamp \
+    -zcvf "$PKG" "${PKGDIR}"
+
+SHA1=$(sha1sum "$PKG" | cut -f1 -d' ')
+echo
+echo "The SHA1 sum of $PKG is $SHA1"
